3c37e1b73204a6c6551333364ad2b6ce88f150f0
FarrasMRazan <ragnaaa6@gmail.com>
2025-12-30 11:37:51 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+// Masukkan ini untuk mematikan cache yang bermasalah di Vercel
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
